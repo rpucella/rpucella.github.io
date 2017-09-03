86d5fed8cbd9ee973d5cb1b93c9f7ffb9447d023
Me <me@example.com>
2017-09-03 10:20:58 -0400

    update h1

---
+++ courses/focs-fa17/homework1.ml
@@ -0,0 +1,90 @@
+(* 
+
+HOMEWORK 1
+
+Name: 
+
+Email:
+
+Remarks, if any:
+
+*)
+
+
+(*
+ *
+ * Please fill in this file with your solutions and submit it
+ *
+ * The functions below are stubs that you should replace with your
+ * own implementation.
+ *
+ * PLEASE DO NOT CHANGE THE TYPES IN THE STUBS BELOW.
+ * Doing so risks making it impossible for me to test your code.
+ *
+ * Always make sure you can #use this file in a FRESH OCaml shell
+ * before submitting it. It has to load without any errors.
+ *
+ *)
+
+
+
+(* Question 1 *)
+
+
+let rec expt (a:int) (b:int):int = 
+  failwith "expt not implemented"
+
+
+let rec choose (n:int) (k:int):int =
+  failwith "choose not implemented"
+
+
+let rec gcd (a:int) (b:int):int  = 
+  failwith "gcd not implemented"
+
+let rec coprimes (n:int):int list = 
+  failwith "coprimes not implemented"
+
+  
+
+(* Question 2 *)
+
+
+let rec tripleUp (xs:'a list):'a list =
+  failwith "tripleUp not implemented"
+
+
+let rec nth (n:int) (xs:'a list):'a =
+  failwith "nth not implemented"
+
+
+let rec last (xs:'a list):'a = 
+  failwith "last not implemented"
+
+
+let rec appendAll (xss:'a list list):'a list =
+  failwith "appendAll not implemented"
+
+
+let rec split (xs:'a list):('a list * 'a list) =
+  failwith "split not implemented"
+  
+
+
+(* QUESTION 3 *)
+
+
+let rec addV (v:int list) (w:int list):int list =
+  failwith "addV not implemented"
+
+
+let rec scaleV (a:int) (v:int list):int list =
+  failwith "scaleV not implemented"
+
+
+let rec inner (v:int list) (w:int list):int =
+  failwith "inner not implemented"
+
+
+let rec outer (v:int list) (w:int list):int list list =
+  failwith "outer not implemented"
