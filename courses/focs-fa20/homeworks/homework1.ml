14cca19903a77559bd0139f58c9006512ee2cc77
Riccardo Pucella <riccardo.pucella@gmail.com>
2020-09-18 08:44:58 -0400

    Homework 1

---
+++ courses/focs-fa20/homeworks/homework1.ml
@@ -0,0 +1,113 @@
+(* 
+
+HOMEWORK 1
+
+Due: Thu Sep 24, 2020 (23h59)
+
+Name: 
+
+Email:
+
+Remarks, if any:
+
+*)
+
+
+(*
+ *
+ * Please fill in this file with your solutions and submit it
+ *
+ * The functions below are stubs that you should replace with your
+ * own implementation.
+ *
+ * PLEASE DO NOT CHANGE THE TYPES IN THE STUBS BELOW.
+ * Doing so risks making it impossible for me to test your code.
+ *
+ * Always make sure you can #use this file in a FRESH OCaml shell
+ * before submitting it. It has to load without any errors.
+ *
+ *)
+
+
+
+(* QUESTION 1 *)
+
+let rec gcd (a:int) (b:int):int  =
+  failwith "Not implemented"
+
+  
+let rec selfConcat (s:string) (n:int):string =
+  failwith "Not implemented"
+
+  
+let rec collatz (a:int):int list =
+  failwith "Not implemented"
+
+  
+let rec range (i:int) (j:int):int list =
+  failwith "Not implemented"
+
+  
+(* QUESTION 2 *)
+
+  
+let rec squares (xs:int list):int list =
+  failwith "Not implemented"
+
+  
+let rec prependString (x:string) (ys:string list):string list = 
+  failwith "Not implemented"
+
+  
+let rec doubleUp (xs:int list):int list =
+  failwith "Not implemented"
+
+  
+let rec nonNegative (xs:int list):int list =
+  failwith "Not implemented"
+
+  
+let rec concatAll (xs: 'a list list): 'a list =
+  failwith "Not implemented"
+
+  
+let rec classify (xs:int list):int list * int list =
+  failwith "Not implemented"
+
+  
+(* QUESTION 3 *)
+
+  
+let rec scale (a:int) (v:int list):int list =
+  failwith "Not implemented"
+
+
+let rec add (v:int list) (w:int list):int list =
+  failwith "Not implemented"
+
+
+let rec length (v:int list): float =
+  failwith "Not implemented"
+
+  
+let rec inner (v:int list) (w:int list):int =
+  failwith "Not implemented"
+        
+
+(* QUESTION 4 *)
+
+  
+let rec set (xs: 'a list): 'a list =
+  failwith "Not implemented"
+
+            
+let rec set_sub (xs:'a list) (ys:'a list): bool = 
+  failwith "Not implemented"
+
+            
+let rec set_union (xs: 'a list) (ys: 'a list): 'a list = 
+  failwith "Not implemented"
+
+              
+let rec set_inter (xs:'a list) (ys:'a list): 'a list = 
+  failwith "Not implemented"
